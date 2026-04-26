01f4b469de0d1c402e0341ad6f33af8b32ca61ad
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2026-04-27 02:17:02 +0530

    time related error fixed

--- backend_link.js
+++ backend_link.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://gocomet-backend-pratyaksh10016605-hbawhdpe.leapcell.dev",
+    baseURL: "http://127.0.0.1:8000",
     headers: {
         "Content-Type": "application/json",
     },
